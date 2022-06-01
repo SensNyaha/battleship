7fbd45d6bf96084af629bb57a9eff8e4d74da53c
Michael <mwmakarov@bk.ru>
2022-06-01 16:52:13 +0400

    fully released choosing possible directions to create a ship. Shipshop and building of ships is not released yet

--- js/main.js
+++ js/main.js
@@ -1,30 +1,31 @@
 'use strict'
 
-let leftPlayerShipsPositions = {byFour: [], byThree: [], byTwo: [], byOne: []};
+let leftPlayerShipsPositions = {
+    byFour: [],
+    byThree: [],
+    byTwo: [],
+    byOne: []
+};
 
 let leftPlayerChosenPositions = [];
 
 let allPlayerDivs = document.querySelectorAll('.playerMap .table');
 
+let direction;
+
 allPlayerDivs.forEach(item => {
 
-    item.querySelectorAll('td').forEach(item => {
-        if (!item.matches('td:first-child') && !item.matches('tr:first-child td')){
-            item.classList.add('possible')
-        }
-    })
-
-    item.addEventListener('click', (event)=>{
-        if (event.target.matches('td') && !event.target.matches('td:first-child') && !event.target.matches('tr:first-child td')){
-            if (event.target.closest('.leftPlayerSide')){
-                if (!event.target.classList.contains('chosen') && leftPlayerChosenPositions.length < 4 && event.target.classList.contains('possible')) {
-                    selectNotChosenPosition(leftPlayerChosenPositions, event.target)
-                    console.log('select')
-                }
-                else if (event.target.classList.contains('chosen')) {
+    item.addEventListener('click', (event) => {
+        if (event.target.matches('td') && !event.target.matches('td:first-child') && !event.target.matches('tr:first-child td')) {
+            if (event.target.closest('.leftPlayerSide')) {
+                if (!event.target.classList.contains('chosen') && leftPlayerChosenPositions.length < 4 && !event.target.classList.contains('unpossible')) {
+                    selectNotChosenPosition(leftPlayerChosenPositions, event.target);
+                    lightUpPossiblePositions(leftPlayerChosenPositions, event.target);
+                    console.log('select', leftPlayerChosenPositions)
+                } else if (event.target.classList.contains('chosen')) {
                     removeAlreadyChosenPosition(leftPlayerChosenPositions, event.target);
-                    returnArrayOfPossiblePositions(leftPlayerChosenPositions, event.target);
-                    console.log('remove')
+                    lightUpPossiblePositions(leftPlayerChosenPositions, event.target);
+                    console.log('remove', leftPlayerChosenPositions)
                 }
             }
         }
@@ -34,38 +35,133 @@ allPlayerDivs.forEach(item => {
 
 // после постройки корабля сделай все не задействованные ячейки possible
 
-function lightUpPossiblePositions(playerPositionsArray, target){
+function lightUpPossiblePositions(playerPositionsArray, target) {
     switch (playerPositionsArray.length) {
         case 0:
             target.closest('tbody').querySelectorAll('td').forEach(item => {
-                if (!item.matches('td:first-child') && !item.matches('tr:first-child td')){
+                if (!item.matches('td:first-child') && !item.matches('tr:first-child td')) {
                     item.classList.remove('unpossible');
-                    item.classList.add('possible');
                 }
             });
             break;
         case 1:
-            //ПРОДОЛЖИ ЛОГИКУ ДЕЙСТВИЙ ПО ОРГАНИЗАЦИИ ПОДСВЕТКИ ВОЗМОЖНЫХ ПОЗИЦИЙ
-            // case 1 будет реализовывать подсветку элементов по 4 сторонам света, когда лишь один элемент в массиве выбранных chosen ячеек
-            // case 2 будет определять какой из элементов (0) или (1) повторяется для определения вертикаль/горизонталь и чтобы далее строить планируемое направление
-            // case 3 работает аналогично, только он ищет минимальное и максимальное значения и от них (-1) и (+1) соответственно
-            // case 4 добавит всем остальным ячейкам класс unpossible
+            makeAllCellsUnpossible(target);
+            unlockPossibleCells(playerPositionsArray, target);
+            break;
+        case 2:
+            makeAllCellsUnpossible(target);
+            unlockPossibleCells(playerPositionsArray, target);
+            break;
+        case 3:
+            makeAllCellsUnpossible(target);
+            unlockPossibleCells(playerPositionsArray, target);
+            break;
+        case 4:
+            makeAllCellsUnpossible(target);
+            break;
     }
-    
+
 }
 
-function selectNotChosenPosition(playerPositionsArray, target){
-    target.style.backgroundColor = 'pink';
+function makeAllCellsUnpossible(target) {
+    target.closest('tbody').querySelectorAll('td').forEach(item => {
+        if (!item.matches('td:first-child') && !item.matches('tr:first-child td') && item !== target && !item.classList.contains('chosen')) {
+            item.classList.add('unpossible');
+        }
+    })
+}
+
+function unlockPossibleCells(playerPositionsArray, target) {
+    switch (playerPositionsArray.length) {
+        case 1:
+            let possibleCellsIndexes = [];
+
+            possibleCellsIndexes.push([playerPositionsArray[0][0] + 1, playerPositionsArray[0][1]]);
+            possibleCellsIndexes.push([playerPositionsArray[0][0], playerPositionsArray[0][1] + 1]);
+            possibleCellsIndexes.push([playerPositionsArray[0][0] - 1, playerPositionsArray[0][1]]);
+            possibleCellsIndexes.push([playerPositionsArray[0][0], playerPositionsArray[0][1] - 1]);
+
+            let filteredPossibleCellsIndexes = possibleCellsIndexes.filter(item => {
+                return item[0] <= 10 && item[1] <= 10
+            });
+
+            let possibleCells = [];
+
+            filteredPossibleCellsIndexes.forEach(item => {
+                possibleCells.push(target.closest('tbody').querySelectorAll('tr')[item[0]].querySelectorAll('td')[item[1]])
+            });
+
+            possibleCells.forEach(item => {
+                item.classList.remove('unpossible');
+            });
+
+            break;
+        case 2:
+            direction = playerPositionsArray[0][0] === playerPositionsArray[1][0] ? 'row' : 'col';
+
+            if (direction === 'row') { //same first number in microarray [n][0]
+                let nextRightCellIndex = Math.max(playerPositionsArray[0][1], playerPositionsArray[1][1]) + 1;
+                let nextLeftCellIndex = Math.min(playerPositionsArray[0][1], playerPositionsArray[1][1]) - 1;
+                if (nextRightCellIndex <= 10){
+                    target.closest('tbody').querySelectorAll('tr')[playerPositionsArray[0][0]].querySelectorAll('td')[nextRightCellIndex].classList.remove('unpossible');
+                }
+                if (nextLeftCellIndex <= 10){
+                    target.closest('tbody').querySelectorAll('tr')[playerPositionsArray[0][0]].querySelectorAll('td')[nextLeftCellIndex].classList.remove('unpossible');
+                }
+            }
+            else{
+                let nextDownCellIndex = Math.max(playerPositionsArray[0][0], playerPositionsArray[1][0]) + 1;
+                let nextUpCellIndex = Math.min(playerPositionsArray[0][0], playerPositionsArray[1][0]) - 1;
+                if (nextDownCellIndex <= 10) {
+                    target.closest('tbody').querySelectorAll('tr')[nextDownCellIndex].querySelectorAll('td')[playerPositionsArray[0][1]].classList.remove('unpossible')}
+                if (nextUpCellIndex<= 10) {
+                    target.closest('tbody').querySelectorAll('tr')[nextUpCellIndex].querySelectorAll('td')[playerPositionsArray[0][1]].classList.remove('unpossible')
+                }
+            }
+            break;
+        case 3:
+            direction = playerPositionsArray[0][0] === playerPositionsArray[1][0] && playerPositionsArray[1][0] === playerPositionsArray[2][0] ? 'row' : 'col';
+            if (direction === 'row') { //same first number in microarray [n][0]
+                let nextRightCellIndex = Math.max(playerPositionsArray[0][1], playerPositionsArray[1][1], playerPositionsArray[2][1]) + 1;
+                let nextLeftCellIndex = Math.min(playerPositionsArray[0][1], playerPositionsArray[1][1], playerPositionsArray[2][1]) - 1;
+                if (nextRightCellIndex <= 10){
+                    target.closest('tbody').querySelectorAll('tr')[playerPositionsArray[0][0]].querySelectorAll('td')[nextRightCellIndex].classList.remove('unpossible');
+                }
+                if (nextLeftCellIndex <= 10){
+                    target.closest('tbody').querySelectorAll('tr')[playerPositionsArray[0][0]].querySelectorAll('td')[nextLeftCellIndex].classList.remove('unpossible');
+                }
+            }
+            else{
+                let nextDownCellIndex = Math.max(playerPositionsArray[0][0], playerPositionsArray[1][0], playerPositionsArray[2][0]) + 1;
+                let nextUpCellIndex = Math.min(playerPositionsArray[0][0], playerPositionsArray[1][0], playerPositionsArray[2][0]) - 1;
+                if (nextDownCellIndex <= 10) {
+                    target.closest('tbody').querySelectorAll('tr')[nextDownCellIndex].querySelectorAll('td')[playerPositionsArray[0][1]].classList.remove('unpossible')}
+                if (nextUpCellIndex<= 10) {
+                    target.closest('tbody').querySelectorAll('tr')[nextUpCellIndex].querySelectorAll('td')[playerPositionsArray[0][1]].classList.remove('unpossible')
+                }
+            }
+            break;
+    }
+}
+
+function selectNotChosenPosition(playerPositionsArray, target) {
     target.classList.add('chosen');
     playerPositionsArray.push(returnMyPosition(target));
 }
 
 function removeAlreadyChosenPosition(playerPositionsArray, target) {
-    target.style.backgroundColor = 'transparent';
     target.classList.remove('chosen');
-    playerPositionsArray.splice(leftPlayerChosenPositions.indexOf(returnMyPosition(target)),1);
+    let currentPosition = playerPositionsArray.filter(item => {
+        let currentTargetPosition = returnMyPosition(target);
+        if (item[0] === currentTargetPosition[0]) {
+            if (item[1] === currentTargetPosition[1]) {
+                return true
+            }
+        }
+    })[0]
+    playerPositionsArray.splice(playerPositionsArray.indexOf(currentPosition), 1);
 }
 
-function returnMyPosition (target) {
-    return [Array.from(target.closest('tr').querySelectorAll('td')).indexOf(target),Array.from(target.closest('tbody').querySelectorAll('tr')).indexOf(target.closest('tr'))]
+function returnMyPosition(target) {
+    return [Array.from(target.closest('tbody').querySelectorAll('tr')).indexOf(target.closest('tr')), Array.from(target.closest('tr').querySelectorAll('td')).indexOf(target)]
 }
